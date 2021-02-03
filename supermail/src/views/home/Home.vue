175417e2836f9f6f7c38ff4a79693d9aa25dbb40
unknown <1761481088@qq.com>
2021-02-03 17:01:15 +0800

    网络模块axios封装及首页数据请求

--- supermail/src/views/home/Home.vue
+++ supermail/src/views/home/Home.vue
@@ -7,11 +7,27 @@
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import {getHomeMultidata} from 'network/home'
   export default {
     name: "Home",
     components: {
       NavBar,
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() { //组件创建时就去请求数据
+      //getHomeMultidata()返回的是promise对象 可以通过.then获取
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list; //将返回的数据保存起来
+        this.recommends = res.data.recommend.list; 
+        
+      })
     }
+  
   }
 </script>
 
