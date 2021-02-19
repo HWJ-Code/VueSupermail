8e4f2f4633bdcdb9676e3c295b54eb0fc030ed0a
unknown <1761481088@qq.com>
2021-02-19 17:06:30 +0800

    首页推荐数据组件

--- supermail/src/views/home/Home.vue
+++ supermail/src/views/home/Home.vue
@@ -2,6 +2,7 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
+    <home-recommend :recommends="recommends"></home-recommend>
   </div>
   
 </template>
@@ -9,6 +10,7 @@
 <script>
   import NavBar from 'components/common/navbar/NavBar.vue'
   import HomeSwiper from './childComps/HomeSwiper.vue'
+  import HomeRecommend from './childComps/HomeRecommend.vue'
 
   import {getHomeMultidata} from 'network/home'
   export default {
@@ -16,6 +18,7 @@
     components: {
       NavBar,
       HomeSwiper,
+      HomeRecommend,
     },
     data() {
       return {
---
+++ supermail/src/views/home/childComps/HomeRecommend.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home-recommend">
+    <div class="recommend-item" v-for="(item,index) in recommends" :key="index"> 
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeRecommend',
+    props: {
+      recommends: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style>
+  .home-recommend {
+    display: flex;
+    font-size: 12px;
+
+    margin-top: 8px;
+    border-bottom: 8px solid #f2f2f2;
+    padding-bottom: 25px;
+  }
+
+  .recommend-item{
+    flex: 1;
+    text-align: center;
+  }
+
+  .recommend-item img{
+    width: 68px;
+    height: 68px;
+    margin-bottom: 8px;
+  }
+
+</style>
